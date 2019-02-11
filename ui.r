d77d17d868aee4cba75411fe01112a67aa76ff0d
ManRod10 <42626680+ManRod10@users.noreply.github.com>
2019-02-11 09:32:57 -0500

    Update ui.r

--- ui.r
+++ ui.r
@@ -17,7 +17,7 @@ shinyUI(fluidPage(
                       cada uno de los colegios, utilizando como insumo las pruebas Saber 11"),
    
         box(selectInput(inputId = "sede",label = "Sede",
-                    choices = unique(datos$cole_nombre_sede),
+                    choices = unique(datos$cole_cod_dane_sede),
                     multiple = FALSE,width = 700))
                                 )
 )
