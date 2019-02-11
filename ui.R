bc3cd09281acba81fd0d56f0c0afc742a6f8133a
ManRod10 <42626680+ManRod10@users.noreply.github.com>
2019-02-10 20:37:22 -0500

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,12 @@
+ui <- fluidPage(
+
+   titlePanel("Generador de Reportes parametrizado"),
+   
+   h4("La siguiente aplicacion genera reportes para 
+                      cada uno de los colegios, utilizando como insumo las pruebas Saber 11"),
+   
+        box(selectInput(inputId = "sede",label = "Sede",
+                    choices = unique(datos$cole_nombre_sede),
+                    multiple = FALSE,width = 700)),
+                    
+   mainPanel(downloadButton("reporte", "Generar")))
