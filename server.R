a65208c445394d75db9e4824b7b0cc16fa6c5dae
ManRod10 <42626680+ManRod10@users.noreply.github.com>
2019-02-10 20:58:59 -0500

    Update server.R

--- server.R
+++ server.R
@@ -6,9 +6,7 @@ shinyServer(function(input, output) {
     content = function(file){
       
       params <- list(make = input$sede)
-        tempReport <- file.path(tempdir(), "Reporte.Rmd")
-        file.copy("Reporte.Rmd", tempReport, overwrite = TRUE)
-      rmarkdown::render(tempReport,output_file = file,
+              rmarkdown::render('Reporte.Rmd',output_file = file,
                         params = params,
                         envir = new.env(parent = globalenv())
                               ) }) 
