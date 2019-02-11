15959d00a2bed57ebdc7fff9746e106f9734b1bf
ManRod10 <42626680+ManRod10@users.noreply.github.com>
2019-02-11 09:49:52 -0500

    Update server.r

--- server.r
+++ server.r
@@ -1,7 +1,18 @@
 library(shiny)
 shinyServer(
   function(input, output) {
-  
+    
+ output$reporte <- downloadHandler(
+    filename = paste("Reporte",".html",sep = ""),
+    
+    content = function(file){
+      
+      params <- list(codigo = input$sede)
+      
+      rmarkdown::render("Reporte.Rmd",output_file = file,
+                        params = params,
+                        envir = new.env(parent = globalenv())
+      ) })  
  
 }
 )
